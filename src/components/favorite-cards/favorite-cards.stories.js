7795e657d3fe4b186131a9cafead850c32c605d7
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-10-05 10:25:23 +0200

    Final Favorite-Cards-Section

---
+++ src/components/favorite-cards/favorite-cards.stories.js
@@ -0,0 +1,9 @@
+import "./favorite-cards.css";
+import favoriteCard from "./favorite-cards.html";
+
+export default {
+  title: "Components/Favorite Cards",
+  parameters: { layout: "centered" },
+};
+
+export const fruits = () => favoriteCard;
