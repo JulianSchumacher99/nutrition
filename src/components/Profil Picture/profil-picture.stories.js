b0ad0cbde7e86d40717c2d6cbec3a71b7591fa04
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-10-07 10:58:38 +0200

    Added Profil-Picture incl. Badge

---
+++ src/components/Profil Picture/profil-picture.stories.js
@@ -0,0 +1,9 @@
+import "./profil-picture.css";
+import profilpicture from "./profil-picture.html";
+
+export default {
+  title: "Components/Profil-Picture",
+  parameters: { layout: "centered" },
+};
+
+export const Avatar = () => profilpicture;
